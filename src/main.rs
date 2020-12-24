9acf73854bc8f7dd3d52b655a5707747547fb3ed
Andreas Karg <andreas@karg.software>
2020-12-24 16:48:24 +0000

    Untested: Transcribe vec3 class + add progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{fs::File, io::Write};
+use std::ops::{Add, Div, DivAssign, Index, Mul, MulAssign, Neg, Sub};
+use {
+    indicatif::ProgressIterator,
+    std::{fs::File, io::Write},
+};
 
 fn main() {
     const IMAGE_WIDTH: i32 = 256;
@@ -8,10 +12,10 @@ fn main() {
 
     writeln!(&file, "P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT).unwrap();
 
-    for j in (0..IMAGE_HEIGHT).rev() {
+    for j in (0..IMAGE_HEIGHT).rev().progress() {
         for i in 0..IMAGE_WIDTH {
-            let r = (i as f64) / ((IMAGE_WIDTH-1) as f64);
-            let g = (j as f64) / ((IMAGE_HEIGHT-1) as f64);
+            let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
             let b = 0.25;
 
             let ir = (255.999 * r) as u8;
@@ -22,3 +26,111 @@ fn main() {
         }
     }
 }
+
+struct Vec3 {
+    e: [f64; 3],
+}
+
+impl Vec3 {
+    pub fn default() -> Self {
+        Self::new(0.0, 0.0, 0.0)
+    }
+    pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
+        Self { e: [e0, e1, e2] }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.e[0]
+    }
+    pub fn y(&self) -> f64 {
+        self.e[1]
+    }
+    pub fn z(&self) -> f64 {
+        self.e[2]
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        let e = self.e;
+        e[0] * e[0] + e[1] * e[1] + e[2] * e[2]
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(
+            self.e[0] - rhs.e[0],
+            self.e[1] - rhs.e[1],
+            self.e[2] - rhs.e[2],
+        )
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(
+            self.e[0] + rhs.e[0],
+            self.e[1] + rhs.e[1],
+            self.e[2] + rhs.e[2],
+        )
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self::new(self.e[0] * rhs, self.e[1] * rhs, self.e[2] * rhs)
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.e[0] *= rhs;
+        self.e[1] *= rhs;
+        self.e[2] *= rhs;
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Self::new(self.e[0] / rhs, self.e[1] / rhs, self.e[2] / rhs)
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        self.e[0] /= rhs;
+        self.e[1] /= rhs;
+        self.e[2] /= rhs;
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Self::new(-self.e[0], -self.e[1], -self.e[2])
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f64;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.e[index]
+    }
+}
+
+type Point3 = Vec3;
+type Color = Vec3;
+
