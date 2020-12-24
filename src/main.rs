06b18e30637b082377f6a7276a2934c0d9fbc803
Andreas Karg <andreas@karg.software>
2020-12-24 16:12:37 +0000

    Write static image to PPM file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{fs::File, io::Write};
+
 fn main() {
-    println!("Hello, world!");
+    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: i32 = 256;
+
+    let file = File::create("image.ppm").unwrap();
+
+    writeln!(&file, "P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT).unwrap();
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r = (i as f64) / ((IMAGE_WIDTH-1) as f64);
+            let g = (j as f64) / ((IMAGE_HEIGHT-1) as f64);
+            let b = 0.25;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+
+            writeln!(&file, "{} {} {}", ir, ig, ib).unwrap();
+        }
+    }
 }
