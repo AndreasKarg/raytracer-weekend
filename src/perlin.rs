017f645114d365a8cd22308d15ed862b057c1413
Andreas Karg <andreas@karg.software>
2020-12-29 21:51:26 +0000

    Add trilinear interpolation

--- src/perlin.rs
+++ src/perlin.rs
@@ -51,10 +51,46 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = (4.0 * p.x()) as i64 as usize & 255;
-        let j = (4.0 * p.y()) as i64 as usize & 255;
-        let k = (4.0 * p.z()) as i64 as usize & 255;
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
-        self.ranfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        let i = p.x().floor() as i64 as usize;
+        let j = p.y().floor() as i64 as usize;
+        let k = p.z().floor() as i64 as usize;
+
+        let mut c = [[[0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.ranfloat[self.perm_x[(i + di) & 255]
+                        ^ self.perm_y[(j + dj) & 255]
+                        ^ self.perm_z[(k + dk) & 255]];
+                }
+            }
+        }
+
+        Self::trilinear_interp(c, u, v, w)
+    }
+
+    fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    let fi = i as f64;
+                    let fj = j as f64;
+                    let fk = k as f64;
+                    accum += (fi * u + (1.0 - fi) * (1.0 - u))
+                        * (fj * v + (1.0 - fj) * (1.0 - v))
+                        * (fk * w + (1.0 - fk) * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+
+        accum
     }
 }
