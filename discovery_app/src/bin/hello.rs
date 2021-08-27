a15d27fc543b1bbf3cb2fe274fb081f3777ef108
Andreas Karg <andreas@karg.software>
2021-08-27 20:02:32 +0100

    Add new (blank) bin firmware to target Discovery board

---
+++ discovery_app/src/bin/hello.rs
@@ -0,0 +1,11 @@
+#![no_main]
+#![no_std]
+
+use discovery_app as _; // global logger + panicking-behavior + memory layout
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    defmt::info!("Hello, world!");
+
+    discovery_app::exit()
+}
